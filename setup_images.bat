03a4449767f43fd14a759591d928205cd59ce278
sadhanamurali65@gmail.com <sadhanamurali65@gmail.com>
2026-04-09 19:15:52 +0530

    change the images

--- setup_images.bat
+++ setup_images.bat
@@ -11,6 +11,7 @@ copy "C:\Users\sadha\.gemini\antigravity\brain\8330c740-e159-4b43-885a-03955347b
 copy "C:\Users\sadha\.gemini\antigravity\brain\8330c740-e159-4b43-885a-03955347b6be\smores_brownie_1775569226656.png" "assets\images\smores_brownie.png"
 copy "C:\Users\sadha\.gemini\antigravity\brain\8330c740-e159-4b43-885a-03955347b6be\baking_brownies_1775567569128.png" "assets\images\baking_brownies.png"
 copy "C:\Users\sadha\.gemini\antigravity\brain\e867ccee-b72d-4949-a6e9-941f2f93286a\kitkat_brownie_1775645693828.png" "assets\images\kitkat_brownie.png"
+copy "C:\Users\sadha\.gemini\antigravity\brain\3661fcb9-765c-48e6-a8c7-188746db964e\media__1775729670622.jpg" "assets\images\walnut_brownie.jpg"
 
 echo Done! The images are now local to your project.
 pause
